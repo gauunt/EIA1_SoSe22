402ef84afa16e03b80bd6c5996a1124f5b0370e3
gauunt <walter.mariesophie@gmail.com>
2022-06-02 17:33:38 +0200

    change

--- Aufgaben/A10/a10.js
+++ Aufgaben/A10/a10.js
@@ -1,1 +1,9 @@
+window.addEventListener("load", function () {
+    document.querySelector(".hinzufügen").addEventListener("click", hinzufügen);
+});
+function hinzufügen() {
+    if (hinzufügentrue == true)
+        document.createElement("li");
+}
+var hinzufügentrue;
 //# sourceMappingURL=a10.js.map
---
+++ Aufgaben/A10/a10.ts
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+    document.querySelector(".hinzufügen").addEventListener("click", hinzufügen);
+});
+
+function hinzufügen(){
+    if(hinzufügentrue==true)
+    document.createElement("li");
+    
+}
+
+var hinzufügentrue: boolean;
