177062cec6f46f1d757315b8d1fb0a5c6fe99d42
gauunt <walter.mariesophie@gmail.com>
2022-10-13 12:23:30 +0200

    add

---
+++ Aufgaben/WiSe22/A01/A01.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=A01.js.map
---
+++ Aufgaben/WiSe22/A01/A01.ts
@@ -0,0 +1,5 @@
+let subjekt: string [] = ["Oma", "Opa", "Lisbet", "Magda", "Erhard"];
+let praedikat: string [] = ["liest", "isst", "rollt", "hüpft", "schleicht"];
+let objekt: string [] = ["Rollmops", "Zeitung", "Tische", "Fische", "Zeit"];
+
+for (let _i: number = subjekt.length; _i
