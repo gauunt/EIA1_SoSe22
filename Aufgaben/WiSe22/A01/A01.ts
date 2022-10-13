60af25becfed36f5517a727d26a30dec26508f2f
gauunt <walter.mariesophie@gmail.com>
2022-10-13 12:36:58 +0200

    Update A01.ts

--- Aufgaben/WiSe22/A01/A01.ts
+++ Aufgaben/WiSe22/A01/A01.ts
@@ -1,5 +1,14 @@
+//Wörter definieren
 let subjekt: string [] = ["Oma", "Opa", "Lisbet", "Magda", "Erhard"];
 let praedikat: string [] = ["liest", "isst", "rollt", "hüpft", "schleicht"];
 let objekt: string [] = ["Rollmops", "Zeitung", "Tische", "Fische", "Zeit"];
 
-for (let _i: number = subjekt.length; _i
+//erstellt Zufallsgedicht
+for (let _i: number = subjekt.length; _i > 0; _i--) {
+    getVerse(subjekt, praedikat, objekt);
+}
+
+//ordnet die Wörter zufällig
+function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]: string{
+
+})
