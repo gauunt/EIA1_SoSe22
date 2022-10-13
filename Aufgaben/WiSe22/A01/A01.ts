4ff5339189f1886cd70c752bcdf9c5089ed70b4d
gauunt <walter.mariesophie@gmail.com>
2022-10-13 14:30:46 +0200

    Update A01.ts

--- Aufgaben/WiSe22/A01/A01.ts
+++ Aufgaben/WiSe22/A01/A01.ts
@@ -11,5 +11,15 @@ for (let _i: number = subjekt.length; _i > 0; _i--) {
 //ordnet die Wörter zufällig
 function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]: string{
 
-    let rNumberS: number = Math.floor(Math.random()*subjekt.length);
+    let rNumberS: number = Math.floor(Math.random() * subjekt.length);
+    let  rNumberP: number = Math.floor(Math.random() * praedikat.length);
+    let rNumberO: number = Math.floor(Math.random() * objekt.length);
+
+    let randomSubjekt: string[] = _subjekt.splice(rNumberS, 1);
+    let randomPraedikat: string[] = _praedikat.splice(rNumberP, 1);
+    let randomObjekt: string[] = _objekt.splice(rNumberO, 1);
+
+    console.log(randomSubjekt + "" + randomPraedikat + "" +randomObjekt);
+    return;
+    
 })
