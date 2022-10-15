fee4df480439c6ffe9b42d5b49cfe353847228e6
gauunt <walter.mariesophie@gmail.com>
2022-10-15 21:16:24 +0200

    Update A01.ts

--- Aufgaben/WiSe22/A01/A01.ts
+++ Aufgaben/WiSe22/A01/A01.ts
@@ -3,13 +3,13 @@ let subjekt: string [] = ["Oma ", "Opa ", "Lisbet ", "Magda ", "Erhard "];
 let praedikat: string [] = ["liest ", "isst ", "rollt ", "hüpft ", "schleicht "];
 let objekt: string [] = ["Rollmops", "Zeitung", "Tische", "Fische", "Zeit"];
 
-//erstellt Zufallsgedicht
+//erstellt Zufallsgedicht Schleife
 for (let _i: number = subjekt.length; _i > 0; _i--) {
-    getVerse(subjekt, praedikat, objekt);
+    getZeile(subjekt, praedikat, objekt);
 }
 
 //ordnet die Wörter zufällig
-function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
+function getZeile(_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
 
     let randomNumberS: number = Math.floor(Math.random() * subjekt.length);
     let  randomNumberP: number = Math.floor(Math.random() * praedikat.length);
@@ -19,7 +19,7 @@ function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]): 
     let randomP: string[] = _praedikat.splice(randomNumberP, 1);
     let randomO: string[] = _objekt.splice(randomNumberO, 1);
 
-    console.log(randomS + "  " + randomP + "  " + randomO);
+    console.log(randomS + " " + randomP + " " + randomO);
     return;
     
 }
