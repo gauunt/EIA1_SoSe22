b5761c99f662ab3ffb7d395bf0be9ded80282c3e
gauunt <walter.mariesophie@gmail.com>
2022-10-15 15:22:56 +0200

    Update A01.ts

--- Aufgaben/WiSe22/A01/A01.ts
+++ Aufgaben/WiSe22/A01/A01.ts
@@ -1,6 +1,6 @@
 //Wörter definieren
-let subjekt: string [] = ["Oma", "Opa", "Lisbet", "Magda", "Erhard"];
-let praedikat: string [] = ["liest", "isst", "rollt", "hüpft", "schleicht"];
+let subjekt: string [] = ["Oma ", "Opa ", "Lisbet ", "Magda ", "Erhard "];
+let praedikat: string [] = ["liest ", "isst ", "rollt ", "hüpft ", "schleicht "];
 let objekt: string [] = ["Rollmops", "Zeitung", "Tische", "Fische", "Zeit"];
 
 //erstellt Zufallsgedicht
@@ -11,15 +11,15 @@ for (let _i: number = subjekt.length; _i > 0; _i--) {
 //ordnet die Wörter zufällig
 function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
 
-    let rNumberS: number = Math.floor(Math.random() * subjekt.length);
-    let  rNumberP: number = Math.floor(Math.random() * praedikat.length);
-    let rNumberO: number = Math.floor(Math.random() * objekt.length);
+    let randomNumberS: number = Math.floor(Math.random() * subjekt.length);
+    let  randomNumberP: number = Math.floor(Math.random() * praedikat.length);
+    let randomNumberO: number = Math.floor(Math.random() * objekt.length);
 
-    let randomSubjekt: string[] = _subjekt.splice(rNumberS, 1);
-    let randomPraedikat: string[] = _praedikat.splice(rNumberP, 1);
-    let randomObjekt: string[] = _objekt.splice(rNumberO, 1);
+    let randomS: string[] = _subjekt.splice(randomNumberS, 1);
+    let randomP: string[] = _praedikat.splice(randomNumberP, 1);
+    let randomO: string[] = _objekt.splice(randomNumberO, 1);
 
-    console.log(randomSubjekt + "" + randomPraedikat + "" + randomObjekt);
+    console.log(randomS + "  " + randomP + "  " + randomO);
     return;
     
 }
