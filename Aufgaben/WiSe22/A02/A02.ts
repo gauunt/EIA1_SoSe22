979e116e13a6b7fffe649d113a426a03cd5ec9f5
gauunt <walter.mariesophie@gmail.com>
2022-10-22 16:58:28 +0200

    change

--- Aufgaben/WiSe22/A02/A02.ts
+++ Aufgaben/WiSe22/A02/A02.ts
@@ -10,38 +10,33 @@ namespace A02 {
 
     window.addEventListener("load", handleLoad);
 
-    // ->Event Listener für click, keyup und mousemove<-
-    //um in der Konsole nur eine Ausgabe jedes Events zu bekommen müsste man .but0,body,.div0/1 löschen, da mit 
-    //document.addEventListener bereits alle Elemente auf der Seite eingeschlossen sind, ich hab aber alle reingenommen weil es 
-    //explizit in der Aufgabe stand.
     function handleLoad(): void {
         document.addEventListener("click", logInfo);
         document.querySelector(".but0").addEventListener("click", clickedBut);
         document.querySelector("body").addEventListener("click", logInfo);
         document.querySelector(".div0").addEventListener("click", logInfo);
         document.querySelector(".div1").addEventListener("click", logInfo);
+
         document.querySelector("body").addEventListener("keyup", logInfo);
         document.querySelector(".div0").addEventListener("keyup", logInfo);
         document.querySelector(".div1").addEventListener("keyup", logInfo);
+
         document.addEventListener("mousemove", setInfoBox);
     }
 
-    // ->bestimmung der x,y Koordinate der Maus, manipulation des Span Elements, bestimmung des targets<-
     function setInfoBox(_event: MouseEvent): void {
 
         let x: number = _event.clientX;
         let y: number = _event.clientY;
         let moveSpan: HTMLElement = document.getElementById("span");
         moveSpan.style.position = "absolute";
-        moveSpan.style.left = x + 10 + "px";
-        moveSpan.style.top = y + 10 + "px";
+        moveSpan.style.left = x + 5 + "px";
+        moveSpan.style.top = y + 5 + "px";
         let mouseTar: EventTarget = _event.target;
         
-        // ->x, y + Target wird in den Span eingefügt<-
         document.querySelector("span").innerHTML = ("x: " + x + " y: " + y + " target: " + mouseTar);
     }
 
-    //->event type, target, current target, event wird bestimmt und in der Konsole bei Klick ausgegeben<-
     function logInfo(_event: Event): void {
     let eventType: string = _event.type;
     let event: Event = _event;
@@ -53,7 +48,6 @@ namespace A02 {
     console.log("Event Type: " + eventType);
     }
 
-    //->fängt DOM-Event ab und gibt es in der Konsole aus<-
     function clickedBut(_event: CustomEvent): void {
         let ePath: EventTarget[] = _event.composedPath();
 
