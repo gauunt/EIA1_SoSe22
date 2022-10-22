ae1226aceccf8536dcc9fa3ae44016225ed41e54
gauunt <walter.mariesophie@gmail.com>
2022-10-22 11:54:45 +0200

    add

--- Aufgaben/WiSe22/A02/A02.js
+++ Aufgaben/WiSe22/A02/A02.js
@@ -1,3 +1,10 @@
+/*
+Aufgabe: 02
+Name: Marie Walter
+Matrikel Nr.: 271138
+Datum: 22.10.2022
+Quellen: Bastian Aberle, Lisa Blindenhöfer
+*/
 var A02;
 (function (A02) {
     window.addEventListener("load", handleLoad);
@@ -10,11 +17,32 @@ var A02;
             document.querySelector("body").addEventListener("keyup", logInfo);
             document.querySelector("div0").addEventListener("keyup", logInfo);
             document.querySelector("div1").addEventListener("keyup", logInfo);
+            document.querySelector("mousemove", setInfoBox);
+            document.querySelector("button").addEventListener("click", button);
         });
     }
-    function setInfoBox() {
+    function setInfoBox(_event) {
+        let ix = _event.clientX;
+        let iy = _event.clientY;
+        console.log([ix, iy]);
+        let hover = document.getElementsByClassName("span");
+        hover.style.position = "absolute";
+        hover.style.left = ix + 10 + "px";
+        hover.style.top = iy + 10 + "px";
+        let cursortarget = _event.target;
+        document.querySelector("span").textContent = ix + "," + " " + iy + "," + "target:" + cursortarget;
     }
-    function logInfo() {
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    }
+    function button(_event) {
+        let path = _event.composedPath();
+        if (path[5] == document) {
+            console.log(_event);
+        }
     }
 })(A02 || (A02 = {}));
 //# sourceMappingURL=A02.js.map
--- Aufgaben/WiSe22/A02/A02.ts
+++ Aufgaben/WiSe22/A02/A02.ts
@@ -1,3 +1,11 @@
+/*
+Aufgabe: 02
+Name: Marie Walter
+Matrikel Nr.: 271138
+Datum: 22.10.2022
+Quellen: Bastian Aberle, Lisa Blindenhöfer
+*/
+
 namespace A02 {
 
     window.addEventListener("load", handleLoad);
@@ -13,14 +21,40 @@ namespace A02 {
             document.querySelector("body").addEventListener("keyup", logInfo);
             document.querySelector("div0").addEventListener("keyup", logInfo);
             document.querySelector("div1").addEventListener("keyup", logInfo);
+
+            document.querySelector("mousemove", setInfoBox);
+
+            document.querySelector("button").addEventListener("click", button);
         })
     }
-    function setInfoBox(): void {
+    function setInfoBox(_event: MouseEvent): void {
+        let ix: number = _event.clientX;
+        let iy: number = _event.clientY;
+        console.log([ix, iy]);
 
+        let hover: HTMLElement = document.getElementsByClassName ("span");
 
+        hover.style.position = "absolute";
+        hover.style.left = ix + 10 + "px";
+        hover.style.top = iy + 10 + "px";
+
+        let cursortarget: EventTarget =_event.target;
+        document.querySelector("span").textContent = ix + "," + " " + iy + "," + "target:" + cursortarget;
     }
-    function logInfo(): void {
 
+    function logInfo(_event: Event): void {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    }
+
+    function button(_event: CustomEvent): void {
+        let path: EventTarget[] = _event.composedPath();
+
+        if (path[5] == document) {
+            console.log(_event);
+        }
     }
 
 }
