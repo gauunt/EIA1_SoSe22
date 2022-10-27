e632408b03c586e47b471333b3c57c7717f0deb8
gauunt <walter.mariesophie@gmail.com>
2022-10-27 14:32:55 +0200

    add

--- Aufgaben/WiSe22/A03/A03.js
+++ Aufgaben/WiSe22/A03/A03.js
@@ -1,94 +1,19 @@
-//globale Variablen 
-var done = 1;
-var openTask = 2;
-var inputDOM;
-var button;
-var todos;
-var counter;
-//Event- Listener
-window.addEventListener("load", function () {
-    inputDOM = document.querySelector("#inputTodo");
-    button = document.querySelector("#addButton");
-    todos = document.querySelector("#tasks");
-    counter = document.querySelector("#totalTasks");
-    button.addEventListener("click", function () {
-        newTask(inputDOM.value);
-    });
-    refreshDOM();
-});
-//Array Todos
-var tasks = [
-    {
-        text: "1kg Äpfel",
-        check: false
-    },
-    {
-        text: "Nudeln",
-        check: true
-    },
-    {
-        text: "Sudokuheft",
-        check: true
+/*
+Aufgabe:02
+Name: Marie Walter
+Matrikel: 271138
+Datum: 22.10.2022
+Quellen: (in Zusammenarbeit mit) Lisa Blindenhöfer, Bastian Aberle
+*/
+var A03;
+(function (A03) {
+    window.addEventListener("load", handleload);
+    function handleload() {
+        document.querySelector("#addButton").addEventListener("click", newitem);
+        console.log("load site");
     }
-];
-function refreshDOM() {
-    todos.innerHTML = "";
-    for (let index = 0; index < tasks.length; index++) {
-        let todo = document.createElement("div");
-        todo.classList.add("todo");
-        todo.innerHTML = "<span class='check " + tasks[index].check + "'><i class='fas fa-check'></i></span>"
-            + tasks[index].text +
-            "<span class='trash fas fa-trash-alt'></span>";
-        todo.querySelector(".check").addEventListener("click", function () {
-            toggleCheckState(index);
-        });
-        todo.querySelector(".trash").addEventListener("click", function () {
-            trashTask(index);
-        });
-        todos.appendChild(todo);
-        const key = inputDOM.value;
-        const storageValue = inputDOM.value;
-        localStorage.setItem(key, storageValue);
+    function newitem() {
+        console.log("create new listelement: name, amout, date");
     }
-    counterRefresh();
-}
-function counterRefresh() {
-    counter.innerHTML = tasks.length + " in total";
-}
-//Neue Aufgabe
-function newTask(text) {
-    if (inputDOM.value != "") {
-        tasks.unshift({
-            text: inputDOM.value,
-            check: false
-        });
-        inputDOM.value = "";
-        openTask++;
-        refreshDOM();
-    }
-}
-function toggleCheckState(index) {
-    if (tasks[index].check == true) {
-        tasks[index].check = false;
-        done--;
-        openTask++;
-    }
-    else if (tasks[index].check == false) {
-        tasks[index].check = true;
-        done++;
-        openTask--;
-    }
-    refreshDOM();
-}
-//lösche task
-function trashTask(index) {
-    if (tasks[index].check == true) {
-        done--;
-    }
-    else if (tasks[index].check == false) {
-        openTask--;
-    }
-    tasks.splice(index, 1);
-    refreshDOM();
-}
+})(A03 || (A03 = {}));
 //# sourceMappingURL=A03.js.map
--- Aufgaben/WiSe22/A03/A03.ts
+++ Aufgaben/WiSe22/A03/A03.ts
@@ -1,116 +1,22 @@
-//globale Variablen 
+/*
+Aufgabe:02
+Name: Marie Walter
+Matrikel: 271138
+Datum: 22.10.2022
+Quellen: (in Zusammenarbeit mit) Lisa Blindenhöfer, Bastian Aberle
+*/
 
-var done: number = 1;
-var openTask: number = 2;
-var inputDOM: HTMLInputElement;
-var button: HTMLElement;
-var todos: HTMLElement;
-var counter: HTMLElement;
+namespace A03{
 
+ window.addEventListener("load", handleload);
 
-//Event- Listener
-window.addEventListener("load", function (): void {
-    inputDOM = document.querySelector("#inputTodo");
-    button = document.querySelector("#addButton");
-    todos = document.querySelector("#tasks");
-    counter = document.querySelector("#totalTasks");
-    button.addEventListener("click", function (): void {
-        newTask(inputDOM.value);
-    });
+ function handleload(): void{
+    document.querySelector("#addButton").addEventListener("click", newitem);
+    console.log("load site")
+ }
 
-    
-    refreshDOM();
-});
-
-//Interface fürs Array
-interface TasksInterface {
-    text: string;
-    check: boolean;
+function newitem(): void{
+    console.log("create new listelement: name, amout, date")
 }
 
-//Array Todos
-var tasks: TasksInterface[] = [
-    {
-        text: "1kg Äpfel",
-        check: false
-    },
-    {
-        text: "Nudeln",
-        check: true
-    },
-    {
-        text: "Sudokuheft",
-        check: true
-    }
-];
-
-
-function refreshDOM(): void {
-    todos.innerHTML = "";
-    for (let index: number = 0; index < tasks.length; index++) {
-        let todo: HTMLElement = document.createElement("div");
-        todo.classList.add("todo");
-        todo.innerHTML = "<span class='check " + tasks[index].check + "'><i class='fas fa-check'></i></span>"
-            + tasks[index].text +
-            "<span class='trash fas fa-trash-alt'></span>";
-
-        todo.querySelector(".check").addEventListener("click", function (): void {
-            toggleCheckState(index);
-        });
-        todo.querySelector(".trash").addEventListener("click", function (): void {
-            trashTask(index);
-        });
-        todos.appendChild(todo);
-        const key: string = inputDOM.value;
-        const storageValue: string = inputDOM.value;
-        localStorage.setItem(key, storageValue);
-    }
-    counterRefresh();
-}
-
-
-
-function counterRefresh(): void {
-    counter.innerHTML = tasks.length + " in total";
-}
-
-//Neue Aufgabe
-function newTask(text: string): void {
-    if (inputDOM.value != "") {
-        tasks.unshift({
-            text: inputDOM.value,
-            check: false
-        });
-        inputDOM.value = "";
-        openTask++;
-        refreshDOM();
-    }  }
-
-
-function toggleCheckState(index: number): void {
-    if (tasks[index].check == true) {
-        tasks[index].check = false;
-        done--;
-        openTask++;
-    }
-    else if (tasks[index].check == false) {
-        tasks[index].check = true;
-        done++;
-        openTask--;
-    }
-
-    refreshDOM();
-}
-
-//lösche task
-function trashTask(index: number): void {
-
-    if (tasks[index].check == true) {
-        done--;
-    }
-    else if (tasks[index].check == false) {
-        openTask--;
-    }
-    tasks.splice(index, 1);
-    refreshDOM();
 }
